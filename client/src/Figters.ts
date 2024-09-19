54bf3b51dc7e5ec4b3e4bb3b28fb339b0e7d0953
Anatoly Novoselov <iseekyouu@yandex.ru>
2024-09-19 12:52:01 +0700

    fighters

---
+++ client/src/Figters.ts
@@ -0,0 +1,29 @@
+import Figter from './Figter'
+
+const fightersRaw = [{
+  avatar: 'https://www.serebii.net/pokemongo/pokemon/001.png',
+  name: 'chekushkin',
+},
+  {
+    avatar: 'https://www.serebii.net/pokemongo/pokemon/001.png',
+    name: 'chekushkin2',
+  },
+  {
+    avatar: 'https://www.serebii.net/pokemongo/pokemon/001.png',
+    name: 'chekushkin3',
+  },
+  {
+    avatar: 'https://www.serebii.net/pokemongo/pokemon/001.png',
+    name: 'chekushkin4',
+  },
+  {
+    avatar: 'https://www.serebii.net/pokemongo/pokemon/001.png',
+    name: 'chekushkin5',
+  },
+  {
+    avatar: 'https://www.serebii.net/pokemongo/pokemon/001.png',
+    name: 'chekushkin6',
+  }
+];
+
+export default fightersRaw.map(fighter => new Figter(fighter.name, fighter.avatar));
---
+++ client/src/Players.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import fighters from './Figters';
+
+interface GridItemProps {
+  id: number;
+}
+
+const GridItem: React.FC<GridItemProps> = ({ id }) => (
+  <div className="bg-blue-500 text-white flex items-center justify-center h-24">
+    Item {id}
+  </div>
+);
+
+const GridLayout: React.FC = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4 p-4">
+      {fighters.map((fighter) => (
+        <GridItem key={fighter.name} id={fighter.name} />
+      ))}
+    </div>
+  );
+};
+
+export default GridLayout;
