562abc24bd9a5a7e4497af6be5c8d2087755ad6d
Dmitriy Botov <dbotov@gmail.com>
2024-09-19 13:15:09 +0700

    feat: stash

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Players from './Players';
+import Fight from './Fight';
+import fighters from './Fighters'
 
 function App() {
+  const [currentScreen, setCurrentScreen] = useState(1);
+  const [player1, setPlayer1] = useState(fighters[0]);
+
+  const renderComponent = () => {
+    switch (currentScreen) {
+      case 0:
+        return <Players />;
+      case 1:
+        return <Fight player1={player1} player2={player1} />;
+      default:
+        return <Players />;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          VORTAL LOMBAT 0.0.0
-        </p>
-        <Players />
-      </header>
+      {renderComponent()}
     </div>
   );
 }
--- client/src/Fight.tsx
+++ client/src/Fight.tsx
@@ -2,22 +2,19 @@ import React from "react";
 import Fighter from "./Fighter";
 
 interface FightProps {
-  fighter1: Fighter;
-  fighter2: Fighter;
-  onFightEnd: (winner: Fighter) => void;
+  player1: Fighter;
+  player2: Fighter;
 }
 
-const Fight: React.FC<FightProps> = ({ fighter1, fighter2, onFightEnd }) => {
+const Fight: React.FC<FightProps> = ({ player1, player2 }) => {
   const handleFight = () => {
-    // Simulate a fight and determine a winner
-    const winner = Math.random() > 0.5 ? fighter1 : fighter2;
-    onFightEnd(winner);
+    console.log('fight!');
   };
 
   return (
     <div>
       <h1>
-        Fight between {fighter1.name} and {fighter2.name}
+        Fight between {player1.name} and {player2.name}
       </h1>
       <button onClick={handleFight}>Start Fight</button>
     </div>
