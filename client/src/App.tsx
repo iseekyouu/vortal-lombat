5e90f3ded69662add29c31d8aaf96f74abb8baf9
Dmitriy Botov <dbotov@gmail.com>
2024-09-19 13:48:29 +0700

    feat: stash handleFight

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Players from './Players';
-import Fight from './Fight';
-import fighters from './Fighters'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Players from "./Players";
+import Fight from "./Fight";
+import fighters from "./Fighters";
 
 function App() {
   const [currentScreen, setCurrentScreen] = useState(1);
   const [player1, setPlayer1] = useState(fighters[0]);
+  const [player2, setPlayer2] = useState(fighters[1]);
 
   const renderComponent = () => {
     switch (currentScreen) {
       case 0:
         return <Players />;
       case 1:
-        return <Fight player1={player1} player2={player1} />;
+        return (
+          <Fight
+            player1={player1}
+            player2={player2}
+            setPlayer1={setPlayer1}
+            setPlayer2={setPlayer2}
+          />
+        );
       default:
         return <Players />;
     }
   };
 
-  return (
-    <div className="App">
-      {renderComponent()}
-    </div>
-  );
+  return <div className="App">{renderComponent()}</div>;
 }
 
 export default App;
--- client/src/Fight.tsx
+++ client/src/Fight.tsx
@@ -1,14 +1,62 @@
 import React from "react";
 import Fighter from "./Fighter";
 
+type Round = {
+  p1dmg: number;
+  p2dmg: number;
+  text: string;
+};
+
 interface FightProps {
   player1: Fighter;
   player2: Fighter;
+  setPlayer1: React.Dispatch<React.SetStateAction<Fighter>>;
+  setPlayer2: React.Dispatch<React.SetStateAction<Fighter>>;
 }
 
-const Fight: React.FC<FightProps> = ({ player1, player2 }) => {
-  const handleFight = () => {
-    console.log('fight!');
+const PlayerSheet: React.FC<{ player: Fighter }> = ({ player }) => {
+  return (
+    <div className="flex flex-col items-center border-indigo-600 border-2">
+      <img src={player.avatar} alt={player.name} />
+      <h2>{player.name}</h2>
+      <p>Health: {player.health}</p>
+      <p>
+        Power: {player.powerMin} - {player.powerMax}
+      </p>
+      <p>Defense: {player.defense}</p>
+      <p>Critical: {player.critical}</p>
+      <p>Evasion: {player.evasion}</p>
+    </div>
+  );
+};
+
+const CombatLog: React.FC<{ log: string[] }> = ({ log }) => {
+  return (
+    <div className="border-indigo-600 border-2 grow">
+      {log.map((entry, index) => (
+        <p key={index}>{entry}</p>
+      ))}
+    </div>
+  );
+};
+
+const Fight: React.FC<FightProps> = ({ player1, player2, setPlayer1, setPlayer2 }) => {
+  const [combatLog, setCombatLog] = React.useState<string[]>([
+    "Fight started",
+    "First round",
+  ]);
+
+  const handleRound = (round: Round) => {
+    setCombatLog([...combatLog, round.text]);
+    setPlayer1((prev) => ({
+      ...prev,
+      health: prev.health - round.p2dmg,
+    }));
+
+    setPlayer2((prev) => ({
+      ...prev,
+      health: prev.health - round.p1dmg,
+    }));
   };
 
   return (
@@ -16,7 +64,22 @@ const Fight: React.FC<FightProps> = ({ player1, player2 }) => {
       <h1>
         Fight between {player1.name} and {player2.name}
       </h1>
-      <button onClick={handleFight}>Start Fight</button>
+      <button
+        onClick={() =>
+          handleRound({
+            p1dmg: 10,
+            p2dmg: 5,
+            text: "Player 1 hits Player 2 for 10 damage",
+          })
+        }
+      >
+        Start Fight
+      </button>
+      <div className="flex w-full bg-zinc-200 px-40">
+        <PlayerSheet player={player1} />
+        <CombatLog log={combatLog} />
+        <PlayerSheet player={player2} />
+      </div>
     </div>
   );
 };
